c79a034a9f6f184dc5e32848c98e5c2dd3b3751a
Thomas Werschnik <Thomas Werschnik>
2017-11-11 16:30:28 +0100

    Add project files.

---
+++ CodingDojo2/Converters/StringToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodingDojo2.Converters
+{
+    public class StringToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string temp = (string)value;
+            /*if ()
+            {
+                return new SolidColorBrush(Colors.);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.);
+            }*/
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
